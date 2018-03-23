fa7331d55218a1ed8f64afb6ca628f70bfd68f61
tylerstruntz <36803513+tylerstruntz@users.noreply.github.com>
2018-03-22 19:26:05 -0500

    added initial files

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+//#include "postalcode.h"
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <cctype>
+#include <cstdio>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+using std::ostream;
+
+using namespace std;
+
+bool check = true;
+
+struct Post
+{
+    string zip;
+    string city;
+    string state;
+    string county;
+    string longitude;
+    string latitude;
+    Post *next;
+    
+}*head, *last_ptr;
+
+void addRecord()
+{
+    fstream data;
+    data.open("asmall.csv", ios::out, ios::app);
+    
+    Post *p;
+    p = new Post;
+    cout <<"Enter zip" << endl;
+    cin >> p -> zip;
+    
+    cout <<"Enter city" << endl;
+    cin >> p -> city;
+    
+    cout <<"Enter state" << endl;
+    cin >> p -> state;
+    
+    cout <<"Enter county" << endl;
+    cin >> p -> county;
+    
+    cout <<"Enter longitude" << endl;
+    cin >> p -> longitude;
+
+    cout <<"Enter latitude" << endl;
+    cin >> p -> latitude;
+    
+    p -> next = NULL;
+    
+}
+
+int main()
+{
+    addRecord();
+    return 0;
+}
